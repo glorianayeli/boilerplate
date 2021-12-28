6b31733cb5baade4da607f79b6e22febd2bee644
glorianayeli100 <glorianayeli_jaime@ucol.com>
2021-12-28 13:31:59 -0600

    adding pug dependency

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 'use strict';
-import * as express from "express"
 require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
@@ -8,16 +7,16 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const app = express();
 app.set('view engine', 'pug');
 
-fccTesting(app); //For FCC testing purposes
+fccTesting(app); // For fCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
+  // Change the response to render the Pug template
   res.render(process.cwd() + '/views/pug');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Listening on port ' + process.env.PORT);
 });
