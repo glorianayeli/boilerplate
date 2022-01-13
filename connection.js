be10f010b6aa3dff0d11e71af3bdee1fdedccd3c
glorianayeli100 <glorianayeli_jaime@ucol.com>
2022-01-13 12:47:56 -0600

    login function

--- connection.js
+++ connection.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
 
+
 async function main(callback) {
     const URI = process.env.MONGO_URI; // Declare MONGO_URI in your .env file
     const client = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -9,15 +10,18 @@ async function main(callback) {
     try {
         // Connect to the MongoDB cluster
         await client.connect();
-
+        
         // Make the appropriate DB calls
         await callback(client);
+        console.log('connected')
 
     } catch (e) {
         // Catch any errors
         console.error(e);
+        console.log("not conection")
         throw new Error('Unable to Connect to Database')
     }
+
 }
 
 module.exports = main;
